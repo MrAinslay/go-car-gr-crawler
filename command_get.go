81307fdfa654952c079a746ab897264b6107ee33
MrAinslay <asteriospaligiannis@gmail.com>
2024-02-16 19:23:15 +0200

    update

--- command_get.go
+++ command_get.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/MrAinslay/go-car-gr-crawler/internal/client"
 )
 
 func commandGet(c client.Client, args ...string) error {
-	url, err := client.GetUrl(args...)
+	url := ""
+	for _, arg := range args {
+		if strings.Contains(arg, "limit=") {
+			strLimit := strings.TrimPrefix(arg, "limit=")
+			limit, err := strconv.Atoi(strLimit)
+			if err != nil {
+				fmt.Printf("Invalid limit defaulting to 1 err: %v", err)
+				limit = 1
+			}
+			for i := range limit {
+				url, err = client.GetUrl(fmt.Sprint(i), args...)
+				if err != nil {
+					return err
+				}
+				if err := c.GetCarPosts(url); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+	}
+
+	url, err := client.GetUrl("", args...)
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Visiting link: %s\n", url)
 	return c.GetCarPosts(url)
 }
--- internal/client/get_url.go
+++ internal/client/get_url.go
@@ -10,28 +10,32 @@ const (
 	baseURL = "https://www.car.gr/used-cars/"
 )
 
-func GetUrl(args ...string) (url string, err error) {
+func GetUrl(page string, args ...string) (url string, err error) {
 	url = baseURL
+
+	if page == "" {
+		page = "1"
+	}
 
 	switch len(args) {
 	case 0:
 		err = errors.New("need atleast one argument")
 	case 1:
-		url = fmt.Sprintf("%s%s.html?activeq=%s&offer_type=sale&pg=1&sort=pra", baseURL, args[0], args[0])
+		url = fmt.Sprintf("%s%s.html?activeq=%s&offer_type=sale&pg=%s&sort=pra", baseURL, args[0], args[0], page)
 	case 2:
 		mileage, err := strconv.Atoi(args[1])
 		if err != nil {
 			err = nil
 			mileage = 150000
 		}
-		url = fmt.Sprintf("%s%s.html?activeq=%s&mileage-to=%d&offer_type=sale&pg=1&sort=pra", baseURL, args[0], args[0], mileage)
+		url = fmt.Sprintf("%s%s.html?activeq=%s&mileage-to=%d&offer_type=sale&pg=%s&sort=pra", baseURL, args[0], args[0], mileage, page)
 	case 3:
 		mileage, err := strconv.Atoi(args[1])
 		if err != nil {
 			err = nil
 			mileage = 150000
 		}
-		url = fmt.Sprintf("%s%s.html?activeq=%s&mileage-to=%d&offer_type=sale&pg=1&sort=%s", baseURL, args[0], args[0], mileage, args[2])
+		url = fmt.Sprintf("%s%s.html?activeq=%s&mileage-to=%d&offer_type=sale&pg=%s&sort=%s", baseURL, args[0], args[0], mileage, page, args[2])
 	default:
 		err = errors.New("too many arguments")
 	}
